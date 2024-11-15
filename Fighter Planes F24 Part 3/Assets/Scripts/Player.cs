8deb83ddc7fb325b2c12b49062432a12b3bbbed2
lunchbox2-2 <166078848+lunchbox2-2@users.noreply.github.com>
2024-11-15 13:40:06 -0500

    power down sound

--- Fighter Planes F24 Part 3/Assets/Scripts/Player.cs
+++ Fighter Planes F24 Part 3/Assets/Scripts/Player.cs
@@ -100,6 +100,7 @@ public class Player : MonoBehaviour
         speed = 6f;
         thruster.gameObject.SetActive(false);
         gameManager.UpdatePowerupText("");
+        gameManager.PlayPowerDown();
     }
 
     IEnumerator ShootingPowerDown()
@@ -107,6 +108,7 @@ public class Player : MonoBehaviour
         yield return new WaitForSeconds(3f);
         shooting = 1;
         gameManager.UpdatePowerupText("");
+        gameManager.PlayPowerDown(); 
     }
 
     private void OnTriggerEnter2D(Collider2D whatIHit)
